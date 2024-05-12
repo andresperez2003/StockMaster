7da173f59ff5df02fb41f81d6d25d0ba8032f821
andresperez2003 <andresap2017@gmail.com>
2024-05-11 22:30:06 -0500

    Fixing configuration

--- src/config.js
+++ src/config.js
@@ -2,12 +2,12 @@ import {config} from 'dotenv'
 
 config()
 
-export const PORT = process.env.PORT || 3000
-export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'andres1004367716'
-export const DB_HOST  = process.env.DB_HOST || 'localhost'
-export const DB_DATABASE  = process.env.DB_DATABASE || 'companydb'
-export const DB_PORT  = process.env.DB_PORT || 3306
+export const PORT = process.env.PORT
+export const DB_USER = process.env.DB_USER
+export const DB_PASSWORD = process.env.DB_PASSWORD
+export const DB_HOST  = process.env.DB_HOST
+export const DB_DATABASE  = process.env.DB_DATABASE
+export const DB_PORT  = process.env.DB_PORT
 
 
 console.log(process.env.PORT)
